642588c6a9b0f39f6e087158601206464677eb8d
MauroPB30 <patinobem93@gmail.com>
2026-05-06 10:38:06 +0200

    fix: #2 replace hardcoded strings with Constants class in handleDataStorageSelection()

---
+++ src/main/java/utils/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author usuario
+ */
+public class Constants {
+    
+    public static final String STORAGE_ARRAYLIST = "ArrayList";
+    public static final String STORAGE_HASHMAP = "HashMap";
+    public static final String STORAGE_FILE = "File";
+    public static final String STORAGE_FILE_SERIALIZATION = "File (Serialization)";
+    public static final String STORAGE_SQL_DATABASE = "SQL - Database";
+    public static final String STORAGE_JPA_DATABASE = "JPA - Database";
+    
+    private Constants(){
+        
+    }
+}
